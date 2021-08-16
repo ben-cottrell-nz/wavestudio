b8cbb7925bff11538a8ca0b6f98b2245679ebe62
Ben Cottrell <bencot@windowslive.com>
2021-08-16 15:04:47 +1200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/wavestudio.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wavestudio.dir/home/ben/Documents/repos/imgui/backends/imgui_impl_opengl3.cpp.o"
+  "CMakeFiles/wavestudio.dir/home/ben/Documents/repos/imgui/backends/imgui_impl_sdl.cpp.o"
+  "CMakeFiles/wavestudio.dir/home/ben/Documents/repos/imgui/examples/libs/gl3w/GL/gl3w.c.o"
+  "CMakeFiles/wavestudio.dir/home/ben/Documents/repos/imgui/imgui.cpp.o"
+  "CMakeFiles/wavestudio.dir/home/ben/Documents/repos/imgui/imgui_demo.cpp.o"
+  "CMakeFiles/wavestudio.dir/home/ben/Documents/repos/imgui/imgui_draw.cpp.o"
+  "CMakeFiles/wavestudio.dir/home/ben/Documents/repos/imgui/imgui_tables.cpp.o"
+  "CMakeFiles/wavestudio.dir/home/ben/Documents/repos/imgui/imgui_widgets.cpp.o"
+  "CMakeFiles/wavestudio.dir/main.cpp.o"
+  "wavestudio"
+  "wavestudio.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/wavestudio.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
